ca69289fbc57b79414e91226cf08dfcd2206039b
rouaguesmi <rouaguesmii@gmail.com>
2022-06-18 15:17:04 +0200

    Task 5 in progress

--- task5.py
+++ task5.py
@@ -1,43 +1,77 @@
 '''
 
-In order to add support to multiple DBMS, one solution is to create 
+In order to add support to multiple DBMS, one solution is to create
 a class that abstracts the database manipulation,
 
-lets create create a classe : TransactionDBManager : this class will 
+lets create create a classe : TransactionDBManager : this class will
 
 '''
 
 import sqlite3
+import psycopg2
+
+HOST_NAME = 'localhost'
+DB_NAME = 'transactions'
+USERNAME = 'postgres'
+PASSWORD = '197382465'
+PORT_ID = '5432'
+
 
 class TransactionDBManager:
+    ''' Abstract database '''
+    def __init__(self, dbtype='sqlite') -> None:
+        self.m_dbtype = dbtype
+        self.m_connection = None
+        self.m_cursor = None
 
-    def __init__(self, db_type='sqlite') -> None:
-        self.db_type = db_type
-        self.connection = None
-        self.cursor = None
-
-
-    def connect_to_db(self, table):
+    def connect_to_db(self, dbname, host=None,
+                      user=None, password=None, port=None):
         ''' Connect to database and create a cursor'''
-        if self.db_type == 'sqlite':
-            self.connection = sqlite3.connect(table)
-            self.cursor = sqlite3.Cursor()
-        elif self.db_type == 'PostgreSql'
-            self.connection = 
-            self.cursor = 
-        elif self.db_type == 'Mysql':
-            self.connection = 
-            self.cursor = 
+        if self.m_dbtype == 'sqlite':
+            self.m_connection = sqlite3.connect(dbname)
+        elif self.m_dbtype == 'PostgreSql':
+            self.m_connection = psycopg2.connect(
+                                    host=host,
+                                    dbname=dbname,
+                                    user=user,
+                                    password=password,
+                                    port=port)
 
-    def execute_query(self, sql:str):
+    def execute_query(self, sql: str):
         ''' ex'''
+        pass
+    
+
+connection = psycopg2.connect(
+                host=HOST_NAME,
+                dbname=DB_NAME,
+                user=USERNAME,
+                password=PASSWORD,
+                port=PORT_ID)
+cursor = connection.cursor()
+
+create_script = ''' CREATE TABLE Devices(
+                        id INTEGER,
+                        device_name TEXT,
+                        PRIMARY KEY(id) );
+                                                              
+                    CREATE TABLE Transactions(
+                        id SERIAL PRIMARY KEY,
+                        datetime INTEGER,
+                        visitor_id INTEGER,
+                        device_type INTEGER,
+                        revenue REAL,
+                        tax REAL,
+                        FOREIGN KEY(device_type) REFERENCES Devices(id)); '''
+                        
+insert_script = ''' INSERT INTO devices 
+'''
+cursor.execute(create_script)
+connection.commit()
+
+cursor.close()
+connection.close()
 
 
 
-SQLITE_DB_MANAGER = TransactionDBManager('sqlite')
 
-SQLITE_DB_MANAGER.execute_query('fffff')
-
-POSGRESQLI_DB_MANAGER = SQLITE_DB_MANAGER('PosGreSQLI')
-
-POSGRESQLI_DB_MANAGER.execute_query('fffff')
