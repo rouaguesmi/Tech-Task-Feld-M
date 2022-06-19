50a6026483734988d965142258d8509bf996dadd
rouaguesmi <rouaguesmii@gmail.com>
2022-06-19 20:42:02 +0200

    Scripts done

--- task5/task5.py
+++ task5/task5.py
@@ -4,7 +4,7 @@ from os import path
 import transactions as tr
 
 # Credentials to connect to postgesql server:
-# Please update these parameters according to your server configuration. 
+# Please update these parameters according to your server configuration.
 
 HOST_NAME = 'localhost'
 DB_NAME = 'transactions'
@@ -14,60 +14,79 @@ PORT_ID = '5432'
 
 DB_NAME_SQLITE = 'transactions.db'
 
+
 def task5_solution():
-    ''' Test the transactions class :
-    1/ Copy the transactions sqlite database into a postgres database
-    2/ Answer task 1 using the transactions class with postgres and sqlite
+    '''
+    In order to demostrate the utility of the class TransactionsDBManager
+    I will use it to do the following tasks :
+
+    1/ Copy the transactions sqlite database into a postgres database.
+    2/ solve task 1 with postgresql and sqlite databases using two
+    objects of the class TransactionsDBManager
+
+    Please refer to the README file or the transactions.py
+    module for more information about the TransactionsDBManager class
     '''
 
+    # the transactions_postgres object will manage the transactions DB
+    #  in a PostgreSQL database server.
     transactions_postgres = tr.TransactionsDBManager(dbtype='postgres')
+    # the transactions_sqlite object will manage the transactions DB
+    #  in a SQLite database server.
     transactions_sqlite = tr.TransactionsDBManager(dbtype='sqlite')
 
     try:
-
+        # Connect to the databse :
         transactions_postgres.connect(DB_NAME, HOST_NAME,
                                       USERNAME, PASSWORD, PORT_ID)
-
         sqlite_dbfile_abspath = path.abspath(DB_NAME_SQLITE)
-
         transactions_sqlite.connect(sqlite_dbfile_abspath)
 
-        
-        create_script = ''' DROP TABLE IF EXISTS transactions, devices;
+        # Sql script to create the tables devices and transactions
+        # in the postgres transactions database.
+        # The database 'transactions' should already be created in the server.
+        create_script = '''
+                        DROP TABLE IF EXISTS transactions, devices;
 
-                            CREATE TABLE Devices(
-                                id INTEGER,
-                                device_name TEXT,
-                                PRIMARY KEY(id) );
+                        CREATE TABLE Devices(
+                            id INTEGER,
+                            device_name TEXT,
+                            PRIMARY KEY(id) );
 
-                            CREATE TABLE Transactions(
-                                id INTEGER PRIMARY KEY,
-                                datetime TEXT,
-                                visitor_id BIGINT,
-                                device_type INTEGER,
-                                revenue REAL,
-                                tax REAL,
-                                FOREIGN KEY(device_type)
-                                REFERENCES Devices(id));'''
+                        CREATE TABLE Transactions(
+                            id INTEGER PRIMARY KEY,
+                            datetime TEXT,
+                            visitor_id BIGINT,
+                            device_type INTEGER,
+                            revenue REAL,
+                            tax REAL,
+                            FOREIGN KEY(device_type)
+                            REFERENCES Devices(id));
+                        '''
 
-        insert_script_devices = ''' INSERT INTO devices (id, device_name)
+        # Sql scripts to insert the data the newly created tables
+        insert_devices = ''' INSERT INTO devices (id, device_name)
                                     VALUES (%s, %s)'''
-        insert_script_transactions = ''' INSERT INTO transactions
+        insert_transactions = ''' INSERT INTO transactions
                                          (id, datetime, visitor_id,
                                          device_type, revenue, tax)
                                          VALUES (%s, %s, %s, %s, %s, %s) '''
 
+        # Get devices and transactions tables data from the sqlite database.
         devices = transactions_sqlite.get_devices()
         transactions = transactions_sqlite.get_transactions()
 
+        # Execute scripts tables creations and data insertion
+        # to the postgreSQL Database :
         transactions_postgres.execute(create_script)
-
-        transactions_postgres.executemany(insert_script_devices, devices)
-        transactions_postgres.executemany(insert_script_transactions, transactions)
-
+        transactions_postgres.executemany(insert_devices, devices)
+        transactions_postgres.executemany(insert_transactions, transactions)
+        # Commit the changes.
         transactions_postgres.commit()
-        
+
+        # Answer task using a PostgreSQL database server :
         transactions_postgres.print_visitor_with_most_revenue()
+        # Answer task using a SQLite database server :
         transactions_sqlite.print_visitor_with_most_revenue()
 
     except Exception as error:
--- task5/transactions.py
+++ task5/transactions.py
@@ -1,9 +1,15 @@
 '''
+Module for task 5.
 
 In order to add support to multiple DBMS, one solution is to create
-a class that abstracts the database manipulation,
+a class that abstracts the database manipulation : connection,
+disconnection, sql execution..
 
-lets create create a classe : TransactionDBManager : this class will
+The class TransactionsDBManager constructor takes into parameters
+the database type : sqlite or postgres in our case.
+
+After that, the class will allow the user to manage
+the two types of databases in the same manner.
 
 '''
 
@@ -13,20 +19,23 @@ import psycopg2
 # Error message to be displayed when the database is disconnected.
 DB_DISCONNECTED_MSG = 'Database is disconnected.'
 
+
 class TransactionsDBManager:
-    ''' Class to manage the 'Transactions' database using different 
-        databases management systems (DBMS). Currently, it supports 
-        SQLite and PostgreSQL DBMS. 
-        But it is easily upgradable to supprot other DBMS which
-        have python libraries that implement the
-        PEP 248 (python database API specifications v2.0)'''
+    '''
+    Class to manage the 'Transactions' database using different
+    databases management systems (DBMS). Currently, it supports
+    SQLite and PostgreSQL DBMS.
+    But it is easily upgradable to supprot other DBMS which
+    can be accessed with a python module that implement the
+    PEP 248 (python database API specifications v2.0)
+    '''
 
     def __init__(self, dbtype='sqlite') -> None:
         self.m_dbtype = dbtype
         self.m_connection = None
         self.m_cursor = None
         self.m_devices_name = 'devices'
-        self.m_transactions_name= 'transactions'
+        self.m_transactions_name = 'transactions'
 
     def connect(self, dbname, host=None,
                 user=None, password=None, port=None):
@@ -49,39 +58,33 @@ class TransactionsDBManager:
         '''Execute a sql query '''
         if self.m_cursor is not None:
             self.m_cursor.execute(sql)
-        else:
-            raise Exception(DB_DISCONNECTED_MSG)
+        raise Exception(DB_DISCONNECTED_MSG)
 
     def fetchall(self, sql: str) -> list:
-        '''Fetch all data returned by the sql query execution.'''
+        '''Fetch all rows after executing the sql query given in parameter'''
         self.execute(sql)
         if self.m_cursor is not None:
             return self.m_cursor.fetchall()
-        else:
-            raise Exception(DB_DISCONNECTED_MSG)
+        raise Exception(DB_DISCONNECTED_MSG)
 
     def fetchone(self, sql: str):
-        ''' Fetch one'''
+        ''' Fetch one row after executing the sql query given in parameter'''
         self.execute(sql)
         if self.m_cursor is not None:
             return self.m_cursor.fetchone()[0]
-        else:
-            raise Exception(DB_DISCONNECTED_MSG)
+        raise Exception(DB_DISCONNECTED_MSG)
 
     def executemany(self, sql: str, data=None) -> None:
-        ''' ExecuteMany ... '''
+        ''' Execute many sql querries with the data given in parameter.'''
         if self.m_cursor is not None:
             self.m_cursor.executemany(sql, data)
-        else:
-            raise Exception(DB_DISCONNECTED_MSG)
+        raise Exception(DB_DISCONNECTED_MSG)
 
     def commit(self) -> None:
-        ''' Commit the updates to the database'''
+        ''' Commit the updates to the database.'''
         if self.m_connection is not None:
             self.m_connection.commit()
-            print('Commit to the database done.')
-        else:
-            print(DB_DISCONNECTED_MSG)
+        raise Exception(DB_DISCONNECTED_MSG)
 
     def close(self) -> None:
         ''' Disconnect from the cursor and the database'''
@@ -92,36 +95,32 @@ class TransactionsDBManager:
         if self.m_connection is not None:
             self.m_connection.close()
             self.m_connection = None
-            print("Connection closed.")
         else:
             print('Connection is already closed.')
 
     @property
     def is_connected(self) -> bool:
-        ''' Return true if the database is connected, false otherwise '''
+        ''' Return true if the database is connected, false otherwise.'''
         return self.m_connection is not None
 
     def get_transactions(self) -> list:
         """Return all the data from the transactions table"""
         if not self.is_connected:
             raise Exception(DB_DISCONNECTED_MSG)
-        else:
-            sql = f'SELECT * FROM {self.m_transactions_name}'
-            return self.fetchall(sql)
+        sql = f'SELECT * FROM {self.m_transactions_name}'
+        return self.fetchall(sql)
 
     def get_devices(self) -> list:
         """Return all the data from the devices table"""
         if not self.is_connected:
             raise Exception(DB_DISCONNECTED_MSG)
-        else:
-            sql = f'SELECT * FROM {self.m_transactions_name}'
-            return self.fetchall(sql)
+        sql = f'SELECT * FROM {self.m_transactions_name}'
+        return self.fetchall(sql)
 
     def print_visitor_with_most_revenue(self):
         ''' Re-implementing task 1 '''
         query = (''' SELECT visitor_id , SUM(revenue) AS visitor_revenue
                      FROM transactions GROUP BY visitor_id
                      ORDER BY visitor_revenue DESC''')
-        print("The visitor's id generating the highest revenue is : ",
+        print("The visitor's id who created the most revenue is : ",
               self.fetchone(query))
-    
