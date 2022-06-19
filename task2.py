207236937597754d2f56a8edc40e7b334d2e6527
rouaguesmi <rouaguesmii@gmail.com>
2022-06-19 13:52:18 +0200

    task 2 method 2

--- task2.py
+++ task2.py
@@ -4,6 +4,20 @@ from datetime import datetime
 import utils
 
 
+def process_result(result: list) -> dict:
+    ''' process result to delete time from date '''
+    # [(2018-10-5 00:12:15, 5041) .... ]
+    new_dict = {}
+    for date_time, tot_revenue in result:
+        date = datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d")
+        if date not in new_dict:
+            new_dict[date] = 0
+        new_dict[date] += tot_revenue
+
+    max_value = max(new_dict, key=new_dict.get)
+    return max_value, new_dict[max_value]
+
+
 def task2_solution():
     """ Find out on which day the most revenue for users
         who ordered via a mobile phone was created """
@@ -18,13 +32,21 @@ def task2_solution():
 
     # execute the sql query and print the result
     result = utils.connect_and_execute_query(task2_query)
+    
+    _date, tot_revenue = process_result(result)
+
+
+    print(f'The day on which the most revenue was created'
+            f' by mobile phone users is: {_date} \n'
+            f'The total generated revenue is: {round(tot_revenue,2)} $ ')
 
     if len(result) > 1 and len(result[0]) > 1:
         date_time, total_revenue = result[0]
         date = datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S")
 
-        print(f'The day on which the most revenue was created by mobile phone users is: {date.strftime("%Y-%m-%d")} \n'
-              f'The total generated revenue is: {round(total_revenue,2)} $ ')
+        print(f'The day on which the most revenue was created'
+                f' by mobile phone users is: {date.strftime("%Y-%m-%d")} \n'
+                f'The total generated revenue is: {round(total_revenue,2)} $ ')
     else:
         print(utils.NO_RESULT_STR)
 
